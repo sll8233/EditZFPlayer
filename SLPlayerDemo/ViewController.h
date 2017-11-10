c22dc541535c982234e26e0837929d4ff0d62de8
sll_ <823317089@qq.com>
2017-11-10 11:00:44 +0800

    Initial Commit

---
+++ SLPlayerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SLPlayerDemo
+//
+//  Created by sll on 2017/11/10.
+//  Copyright © 2017年 孙乐乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SLPlayerDemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  SLPlayerDemo
+//
+//  Created by sll on 2017/11/10.
+//  Copyright © 2017年 孙乐乐. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
