78166a01afb3a679f6b31040fd0d9621cf560525
sll_ <823317089@qq.com>
2017-11-10 15:40:10 +0800

    修改之前

---
+++ SLPlayerDemo/MovieViewContr.h
@@ -0,0 +1,14 @@
+//
+//  MovieViewContr.h
+//  SLPlayerDemo
+//
+//  Created by sll on 2017/11/10.
+//  Copyright © 2017年 孙乐乐. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MovieViewContr : UIViewController
+/** 视频URL */
+@property (nonatomic, strong) NSURL *videoURL;
+@end
--- SLPlayerDemo/ViewController.m
+++ SLPlayerDemo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "MovieViewContr.h"
 @interface ViewController ()
 
 @end
@@ -17,12 +17,18 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+   
+   
 }
 
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event{
+    UIStoryboard *bs =  [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    MovieViewContr *vc = [bs instantiateViewControllerWithIdentifier:@"MovieViewContr"];
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    vc.videoURL = [NSURL URLWithString:@"http://baobab.wdjcdn.com/1456734464766B(13).mp4"];
+    [self.navigationController pushViewController:vc
+                                         animated:YES];
 }
 
 
